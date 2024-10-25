7712c4bf97cd6d694444353c4b218b42d680d468
ClementLmd <clement.laumond@gmail.com>
2024-10-25 16:59:28 +0200

    refacto header

--- front/components/HeaderLayout.tsx
+++ front/components/HeaderLayout.tsx
@@ -4,8 +4,13 @@ import Image from 'next/image';
 import { routes } from '../app/config/routes';
 import DesktopNavBar from './DesktopNavBar';
 import MobileNavBar from './MobileNavBar';
+import { useState } from 'react';
 
 export default function HeaderLayout() {
+  const [hasAccessToWindow, setHasAccessToWindow] = useState<boolean>(false);
+  if (typeof window !== 'undefined') {
+    setHasAccessToWindow(true);
+  }
   return (
     <div className={styles.body}>
       <div id="headerLogo">
@@ -20,7 +25,9 @@ export default function HeaderLayout() {
           />
         </Link>
       </div>
-      <div id="headerLinks">{window.innerWidth > 865 ? <DesktopNavBar /> : <MobileNavBar />}</div>
+      <div id="headerLinks">
+        {hasAccessToWindow && window.innerWidth > 865 ? <DesktopNavBar /> : <MobileNavBar />}
+      </div>
       <div className={styles.login}>Connexion</div>
     </div>
   );
